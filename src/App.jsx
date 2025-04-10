e516ac0ab4ef27c2695f18d4be0f4d2f1ec57ab5
Nikolas Henrik Owen <niko.owen88@gmail.com>
2025-04-10 20:38:47 +0700

    create: filter todo using reducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,38 @@ import Navbar from './layout/Navbar'
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import EditModal from './components/EditModal';
+import { useReducer } from 'react';
 
 
+const initialTodoState = {
+  filter: 'semua',
+};
+
+function reducer(todoState, action) {
+  switch (action.type) {
+    case 'semua':
+      return { filter: 'semua' };
+    case 'selesai':
+      return { filter: 'selesai' };
+    case 'aktif':
+      return { filter: 'aktif' };
+    default:
+      return todoState;
+  }
+}
 
 function App() {
   const [todos, setTodos] = useState([])
+
+  const [todoState, dispatch] = useReducer(reducer, initialTodoState);
+
+  // mengisi filteredTodos dengan todo, sesuai dengan todoState yang diset dari reducer
+  const filteredTodos = todos.filter((todo) => {
+    if (todoState.filter === 'semua') return true;
+    if (todoState.filter === 'selesai') return todo.completed;
+    if (todoState.filter === 'aktif') return !todo.completed;
+    return true;
+  });
 
   const addTodo = (text) => {
     const newTodo = {
@@ -51,16 +78,29 @@ function App() {
     setEditingText("");
     document.getElementById("edit-modal").classList.add("hidden");
   };
-
+  
   return (
     <>
       <Navbar/>
       <div className='text-center container mx-auto'>
         <h1 className='text-2xl font-bold mt-5'>Todo List</h1>
         <AddTodo addTodo={addTodo} />
-        
+
+        {/* Menggunakan reducer untuk mengset state dari todo yang ingin ditampilkan */}
+        <div class="inline-flex rounded-md shadow-xs mt-5" role="group">
+          <button onClick={() => dispatch({ type: 'semua' })} type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            Semua
+          </button>
+          <button onClick={() => dispatch({ type: 'selesai' })} type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            Selesai
+          </button>
+          <button onClick={() => dispatch({ type: 'aktif' })} type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            Aktif
+          </button>
+        </div>
+
         <TodoList
-          todos={todos}
+          todos={filteredTodos}
           toggleTodo={toggleTodo}
           deleteTodo={deleteTodo}
           setEditingId={setEditingId}
